f6c56496ef720dee2d6b7e1fc6118699b8dc2389
CouldIn <Lexachpr0@gmail.com>
2019-06-07 14:20:38 +0600

    Initial commit

---
+++ RGR_PROECTIROVANIE/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RGR_PROECTIROVANIE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.ButtonConvert = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonOpen
+            // 
+            this.ButtonOpen.Location = new System.Drawing.Point(10, 12);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(100, 25);
+            this.ButtonOpen.TabIndex = 0;
+            this.ButtonOpen.Text = "OpenFile";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
+            this.ButtonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
+            // 
+            // ButtonConvert
+            // 
+            this.ButtonConvert.Location = new System.Drawing.Point(10, 43);
+            this.ButtonConvert.Name = "ButtonConvert";
+            this.ButtonConvert.Size = new System.Drawing.Size(100, 25);
+            this.ButtonConvert.TabIndex = 1;
+            this.ButtonConvert.Text = "Convert";
+            this.ButtonConvert.UseVisualStyleBackColor = true;
+            this.ButtonConvert.Click += new System.EventHandler(this.ButtonConvert_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(120, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(472, 436);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(120, 436);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 436);
+            this.Controls.Add(this.ButtonConvert);
+            this.Controls.Add(this.ButtonOpen);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "(ASCII)lisator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonOpen;
+        private System.Windows.Forms.Button ButtonConvert;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
